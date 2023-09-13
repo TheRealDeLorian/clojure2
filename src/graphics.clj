9a66aaa6765945a4dfcb97cac6cdc2b10f59a392
Dorian C <thenewdorian21@gmail.com>
2023-09-13 17:59:49 -0600

    makes a white square

--- src/graphics.clj
+++ src/graphics.clj
@@ -2,41 +2,35 @@
   (:require [clojure.test :refer :all])
   (:require [quil.core :as q]))
 
-
-(with-test
-  (defn add [a b]
-    (- a b))
-  (is (= 3 (add 1 2)))
-  (is (= 7 (add 3 4))))
-
-
-(defn do-something [opts]
-  (println "hello"))
-
-(defn tests [opts]
-  (run-all-tests))
-
-
-
-
 (defn setup []
-  (q/frame-rate 1)                    ;; Set framerate to 1 FPS
-  (q/background 200))                 ;; Set the background colour to
-                                      ;; a nice shade of grey.
+  (q/frame-rate 100)                    
+  (q/background 200))                
+                                      
 (defn draw []
-  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
+  (let [x 0
+        y 0]
+    (q/rect x y 10 10)))
 
-  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/rect x y x y)))         ;; Draw a circle at x y with the correct diameter
+(defn update [state]
+  (update-in state [:r] inc))
+
 
 (defn draw-stuff [opts]
-  (q/defsketch example                  ;; Define a new sketch named example
-    :title "Oh so many gray rectangles"    ;; Set the title of the sketch
-    :settings #(q/smooth 2)             ;; Turn on anti-aliasing
+  (q/defsketch example                 ;; Define a new sketch named example
+    :title "Game of Life"    ;; Set the title of the sketch
     :setup setup                        ;; Specify the setup fn
-    :draw draw                          ;; Specify the draw fn
-    :size [323 200]))                   ;; You struggle to beat the golden ratio
+    :draw draw   
+    :update update
+    :size [1000 1000]))                   ;; You struggle to beat the golden ratio
+  
+
+
+
+ ;   (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
+  ; (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
+   ;(q/fill (q/random 255))               ;; Set the fill colour to a random grey
+  
+  ; (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
+   ;      x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
+    ;     y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
+    ; (q/rect x y x y)))         ;; Draw a circle at x y with the correct diameter
